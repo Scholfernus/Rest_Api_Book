eb2b7bc2ca4d89e834ad9487b6bdd613e1f1f723
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-10-05 22:12:54 +0200

    Book service i main

---
+++ src/main/java/com/example/demo10/service/BookService.java
@@ -0,0 +1,18 @@
+package com.example.demo10.service;
+
+import com.example.demo10.model.BookModel;
+import com.example.demo10.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+    private final BookRepository bookRepository;
+
+    public List<BookModel> getBookLIst() {
+        return bookRepository.findAll();
+    }
+}
