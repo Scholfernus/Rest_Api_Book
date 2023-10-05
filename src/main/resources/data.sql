b67173816387ea05fb5c415a3bdcac58458003c4
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-10-05 22:11:14 +0200

    Properties i data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO book_model(title, author, year_of_issue)
+VALUES ('Quo Vadis','Sienkiewicz','1897');
+
+INSERT INTO book_model(title, author, year_of_issue)
+VALUES ('Da Vinci Code','Dan Brown','2015');
+
+INSERT INTO book_model(title, author, year_of_issue)
+VALUES ('Lord Jim','Byron','1888')
