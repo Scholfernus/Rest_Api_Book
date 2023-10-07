f7787393e49587263b33ed86fdd9cc06c7a58e8e
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-10-07 22:10:53 +0200

    BookRepository zmiany

--- src/main/java/com/example/demo10/repository/BookRepository.java
+++ src/main/java/com/example/demo10/repository/BookRepository.java
@@ -6,26 +6,29 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Book;
 import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<BookModel, Long> {
 
-/* pobieranie wszystkich książek autora*/
+    /* pobieranie wszystkich książek autora*/
     @Query("SELECT a FROM BookModel a WHERE a.author = :author")
     List<BookModel> findAllBooksByAuthor(@Param("author") String author);
+    List<BookModel> findByAuthor(@Param("author") String author);
+
 
     /*Pobieranie książek wydanych w danym roku*/
     @Query("SELECT y FROM BookModel y WHERE y.yearOfIssue = :yearOfIssue")
-    List<BookModel> findBooksByYearOfIssue(@Param("yearOfIssue")Integer yearOfIssue);
-//    List<BookModel> findByYearOfIssueGreaterThan(Integer year);
+    List<BookModel> findBooksByYearOfIssue(@Param("yearOfIssue") Integer yearOfIssue);
+
+    //    List<BookModel> findByYearOfIssueGreaterThan(Integer year);
     /*Pobieranie książek po określonym tytule*/
     @Query("SELECT t FROM BookModel t WHERE t.title = :title ")
-    List<BookModel> findBooksByTitle(@Param("title")String title);
+    List<BookModel> findBooksByTitle(@Param("title") String title);
 
     /*Pobieranie wszystkich książek, których autor rozpoczyna się od danej litery*/
     List<BookModel> findByAuthorStartingWith(String letter);
-/*Pobieranie książek wydanych przed określonym rokiem*/
+
+    /*Pobieranie książek wydanych przed określonym rokiem*/
     List<BookModel> findByYearOfIssueBefore(Integer year);
 }
