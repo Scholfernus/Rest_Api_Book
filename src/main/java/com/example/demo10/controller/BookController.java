686fc8542a51e2f3205c1975ddb170dbaccdd0ec
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-10-07 22:10:05 +0200

    BookController zmiany

--- src/main/java/com/example/demo10/controller/BookController.java
+++ src/main/java/com/example/demo10/controller/BookController.java
@@ -6,10 +6,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -19,7 +21,7 @@ public class BookController {
 
     private final BookService bookService;
 
-    @GetMapping
+    @GetMapping// pobieranie wszystkich książek
     public ResponseEntity<List<BookModel>> getBookList() {
         try {
             List<BookModel> books = bookService.getBookLIst();
@@ -29,4 +31,22 @@ public class BookController {
         }
     }
 
+    @GetMapping("/author/{author}")
+    public ResponseEntity<List<BookModel>> getBooksByAuthor(@PathVariable String author) {
+        try {
+            List<BookModel> books = bookService.getAllBooksByAuthor(author);
+            return ResponseEntity.ok(books);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Optional<BookModel>> getBookById(@PathVariable Long id){
+        try {
+            Optional<BookModel> bookById = bookService.getBookById(id);
+            return ResponseEntity.ok(bookById);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 }
